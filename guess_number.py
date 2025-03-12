7c2120da50a2d9fd088dfaea115217c021a6f250
pawan-2001-bsc <pavanbatman8@gmail.com>
2025-03-12 16:09:33 +0530

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -24,3 +24,35 @@
 import random   # Python module named random
 
 random_number = random.randint(1,20) # Generate a random number between 1 and 20
+
+
+import random
+
+random_number=random.randint(1,21)
+
+count=0
+
+max_guesses=5
+
+while count<max_guesses:
+    user_guess=int(input("Enter your guess:"))
+    count+=1
+
+    if user_guess<1 or user_guess>20:
+        print("Invalid input")
+        continue
+
+
+    elif user_guess< random_number:
+        print("too low")
+
+    elif user_guess>random_number:
+        print("too high")
+
+
+    
+    else:
+        print("Congratulations!, You have guessed correctly")
+
+if count == max_guesses and user_guess != random_number:
+    print("Game Over! The correct number was:",random_number
