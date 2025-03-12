9f78e915170bdddad919410b7ab73614bc414402
ashish-panicker <ashish.s.panicker@proton.me>
2025-03-12 16:17:12 +0530

    missing number

--- single_sum.py
+++ single_sum.py
@@ -20,3 +20,12 @@ Example
   Output: 2
 
 """
+
+def digital_root(n):
+    while n >= 10:
+        n = sum(int(digit) for digit in str(n))
+    return n
+
+num = int(input("Enter a number: "))
+result = digital_root(num)
+print("Output:", result)
