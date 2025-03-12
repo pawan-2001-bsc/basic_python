0ba72489b5bd1136d605967cea576681657770b0
pawan-2001-bsc <pavanbatman8@gmail.com>
2025-03-12 16:10:47 +0530

    Update prime_range.py

--- prime_range.py
+++ prime_range.py
@@ -5,3 +5,25 @@ Example:
   For start = 10, end = 30:
   11, 13, 17, 19, 23, 29
 """
+
+
+start=int(input("Enter starting number:"))
+
+end=int(input("Enter ending number:"))
+
+
+
+for i in range(start,end):
+    if i<2:
+        continue
+
+    is_prime=True
+
+    for j in range(2,i):
+        if i%j==0:
+            is_prime=False
+            break
+
+
+    if is_prime:
+        print(i)
